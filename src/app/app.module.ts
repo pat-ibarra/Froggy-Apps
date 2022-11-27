c4be9be690fc9b8166190fe6d803c964df3deba9
MaxMorales727677 <jesus.morales.pintor@u-erre.mx>
2022-11-27 10:17:10 -0600

    Componente Login - Max

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { RegisterComponent } from './components/register/register.component';
+import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
+import { RecuperarContrasenaComponent } from './components/recuperar-contrasena/recuperar-contrasena.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CatalogoComponent,
+    RegisterComponent,
+    VerificarCorreoComponent,
+    RecuperarContrasenaComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
