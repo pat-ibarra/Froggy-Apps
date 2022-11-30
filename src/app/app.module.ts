9983fb2479fd0e2568a73c4bdef7422f3cd93841
MaxMorales727677 <jesus.morales.pintor@u-erre.mx>
2022-11-29 19:43:46 -0600

    Agregar Registro y Recuperar Contraseña

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Módulos
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Componentes 
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
@@ -22,7 +27,8 @@ import { SpinnerComponent } from './shared/spinner/spinner.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
