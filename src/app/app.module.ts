dc240784f5229a886491ae5039d2db22556f8aa8
Ana Morin <mychelyana@gmail.com>
2022-12-07 17:39:30 -0600

    still trying :(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ToastModule } from 'primeng/toast';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
+// import { ToastModule } from 'primeng/toast';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { ButtonModule } from 'primeng/button';
 
 //Componentes
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { LoginComponent } from './components/login/login.component';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
 import { RegisterComponent } from './components/register/register.component';
 import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
 import { RecuperarContrasenaComponent } from './components/recuperar-contrasena/recuperar-contrasena.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
-
 import { environment } from 'src/environments/environment';
 import { ListComponent } from './components/list/list.component';
 import { CreateArtComponent } from './components/create-art/create-art.component';
 import { NbarComponent } from './components/nbar/nbar.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
